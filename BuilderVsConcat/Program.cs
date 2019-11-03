0bed081b0cc37d08f9852d9ba031922da9411566
Takayuki Kondo <tkondou@gmail.com>
2019-11-04 01:24:12 +0900

    :+1: StringBuilder,Append VS String.ConCat

---
+++ BuilderVsConcat/Program.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace BuilderVsConcat
+{
+    public class BuilderVsConcat
+    {
+        private readonly int N = 10000;
+
+        [Benchmark]
+        public string StringConcat()
+        {
+            return string.Concat(Enumerable.Repeat("a", N).ToArray());
+        }
+
+        [Benchmark]
+        public string StringBuilder()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('a', N);
+            return sb.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BuilderVsConcat>();
+        }
+    }
+}
